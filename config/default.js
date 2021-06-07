5dd6e184c3bc35444ca91ffacad182d6b18c1a55
Maxim Piven <shpax1@gmail.com>
2021-06-07 16:59:32 +0300

    + sudo

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   exec: {
     scripts: {
       "init-cam": {
-        run: "service uv4l_raspicam restart",
+        run: "sudo service uv4l_raspicam restart",
       },
       test: {
         run: 'echo "this is a script test"',
