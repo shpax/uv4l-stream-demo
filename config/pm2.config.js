7f0e9a782fa27d7612d67ebab489143049998253
Piven Maksym <shpax1@gmail.com>
2021-06-07 17:17:43 +0300

    rm pm2 routing

--- config/pm2.config.js
+++ config/pm2.config.js
@@ -1,10 +1,8 @@
-const path = require("path");
-
 module.exports = {
   apps: [
     {
       name: "console-exec-service",
-      script: path.resolve(__dirname, "../", "bin/www"),
+      script: `bin/www`,
       restart_delay: 500,
       watch: ["config"],
       watch_options: {
